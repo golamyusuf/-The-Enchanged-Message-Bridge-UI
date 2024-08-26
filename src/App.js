6619da24d826ea76408d5c4eee665b25d4fd7258
Golam Yusuf <yusuf_siffo@yahoo.com>
2024-08-26 21:55:17 +0600

    File store and file retrive is done. Showing the history data properly.

--- src/App.js
+++ src/App.js
@@ -67,6 +67,9 @@ const App = () => {
       {!!user ?
         (
           <>
+            <div className="user-info">
+              <h2>Logged in as: {user.username}</h2>
+            </div>
             <SockJsClient
               url={SOCKET_URL}
               topics={['/topic/group']}
@@ -75,6 +78,7 @@ const App = () => {
               onMessage={msg => onMessageReceived(msg)}
               debug={false}
             />
+           
             <Messages
               messages={messages}
               currentUser={user}
@@ -89,94 +93,4 @@ const App = () => {
 }
 
 export default App;
-
-
-// import React, { useState } from 'react';
-// import SockJsClient from 'react-stomp';
-// import './App.css';
-// import Input from './components/Input/Input';
-// import LoginForm from './components/LoginForm';
-// import Messages from './components/Messages/Messages';
-// import chatAPI from './services/chatapi';
-// import { randomColor } from './utils/common';
-
-
-// const SOCKET_URL = 'http://localhost:8080/ws-chat/';
-
-// const App = () => {
-//   const [messages, setMessages] = useState([])
-//   const [user, setUser] = useState(null)
-
-//   let onConnected = () => {
-//     console.log("Connected!!")
-//   }
-
-//   let onMessageReceived = (msg) => {
-//     console.log('New Message Received!!', msg);
-//     setMessages(messages.concat(msg));
-//   }
-
-//   let onSendMessage = (msgText, file) => {
-//     const formData = new FormData();
-//     formData.append('sender', user.username);
-//     formData.append('content', msgText);
-
-//     if (file) {
-//         formData.append('file', file);
-//     }
-//    //NEED TO REMOVE
-   
-//     for (let [key, value] of formData.entries()) {
-//       if (value instanceof File) {
-//           console.log(`${key}: ${value.name}, ${value.type}`);
-//       } else {
-//           console.log(`${key}: ${value}`);
-//       }
-//   }
-//   //NEED TO REMOVE
-//     chatAPI.sendMessage(formData)
-//         .then(res => {
-//             console.log('Sent', res);
-//         })
-//         .catch(err => {
-//             console.log('Error Occurred while sending message to API', err);
-//         });
-// }
-
-//   let handleLoginSubmit = (username) => {
-//     console.log(username, " Logged in..");
-
-//     setUser({
-//       username: username,
-//       color: randomColor()
-//     })
-
-//   }
-
-//   return (
-//     <div className="App">
-//       {!!user ?
-//         (
-//           <>
-//             <SockJsClient
-//               url={SOCKET_URL}
-//               topics={['/topic/group']}
-//               onConnect={onConnected}
-//               onDisconnect={console.log("Disconnected!")}
-//               onMessage={msg => onMessageReceived(msg)}
-//               debug={false}
-//             />
-//             <Messages
-//               messages={messages}
-//               currentUser={user}
-//             />
-//             <Input onSendMessage={onSendMessage} />
-//           </>
-//         ) :
-//         <LoginForm onSubmit={handleLoginSubmit} />
-//       }
-//     </div>
-//   )
-// }
-
-// export default App;
+ 
